3c19bbf7eb9747d67535a30e698bf18fec17a10b
1119778139@qq.com <qp981003>
2019-01-27 17:13:44 +0800

    内容服务的发布

--- taotao-rests/src/main/java/com/taotao/rests/controller/ContentController.java
+++ taotao-rests/src/main/java/com/taotao/rests/controller/ContentController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -24,13 +25,16 @@ public class ContentController {
 
     @Autowired
     private ContentService contentService;
+
     @RequestMapping("/list/{contentCategoryId}")
-    public TaotaoResult getContentList(@PathVariable long contentCategoryId){
+    @ResponseBody
+    public TaotaoResult getContentList(@PathVariable Long contentCategoryId){
         try {
             List<TbContent> list = contentService.getContentList(contentCategoryId);
             return TaotaoResult.ok(list);
         }catch (Exception e){
-            e.getStackTrace();
+//            捕获异常 后台打印
+            e.printStackTrace();
 //            错误500 并且返回一个字符串 使用ExceptionUtil工具类 返回e
             return TaotaoResult.build(500, ExceptionUtil.getStackTrace(e));
         }
