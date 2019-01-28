35dbb15234798f18c0d05a0a45497b2e16decc47
1119778139@qq.com <qp981003>
2019-01-28 16:14:27 +0800

    实现首页大广告位的展示

--- taotao-portal/src/main/java/com/taotao/portal/controller/IndexController.java
+++ taotao-portal/src/main/java/com/taotao/portal/controller/IndexController.java
@@ -1,18 +1,28 @@
 package com.taotao.portal.controller;
 
+import com.taotao.portal.service.ContentService;
 import com.taotao.utils.TaotaoResult;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.jws.WebParam;
+
 @Controller
 public class IndexController {
 
-    @RequestMapping("/index")
+    @Autowired
+    private ContentService contentService;
 
-    public String showIndex(){
+    @RequestMapping("/index")
+    public String showIndex(Model model){
+        String adjson = contentService.getContentList();
+        model.addAttribute("ad1",adjson);
         return "index";
+
     }
 
     @RequestMapping(value = "/httpclient/post",method = RequestMethod.POST)
