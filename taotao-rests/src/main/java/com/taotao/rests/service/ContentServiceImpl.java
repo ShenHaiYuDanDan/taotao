4a0ae572a385f44da721b8741998bf8891941c2f
1119778139@qq.com <qp981003>
2019-02-01 19:26:03 +0800

    实现首页大广告位的展示

--- taotao-rests/src/main/java/com/taotao/rests/service/ContentServiceImpl.java
+++ taotao-rests/src/main/java/com/taotao/rests/service/ContentServiceImpl.java
@@ -1,9 +1,13 @@
 package com.taotao.rests.service;
 
+import org.apache.commons.lang3.StringUtils;
 import com.taotao.mapper.TbContentMapper;
 import com.taotao.pojo.TbContent;
 import com.taotao.pojo.TbContentExample;
+import com.taotao.rests.dao.JedisClient;
+import com.taotao.utils.JsonUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
   /*      内容管理
@@ -16,21 +20,43 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 @Service
 public class ContentServiceImpl  implements ContentService {
-    
+
     @Autowired
     private TbContentMapper tbContentMapper;
+    @Autowired
+    private JedisClient jedisClient;
 
+    @Value("${INDEX_CONTENT_REDIS_KEY}")
+    private String INDEX_CONTENT_REDIS_KEY;
     @Override
     public List<TbContent> getContentList(long contentCid) {
+       //从缓存中取内容
+        try {
+            String result = jedisClient.hget(INDEX_CONTENT_REDIS_KEY, contentCid + "");
+            if (!StringUtils.isBlank(result)) {
+                //把字符串转换成list
+                List<TbContent> resultList = JsonUtils.jsonToList(result, TbContent.class);
+                return resultList;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        
 //        根据id查询列表
         TbContentExample example=new TbContentExample();
         TbContentExample.Criteria criteria = example.createCriteria();
         criteria.andCategoryIdEqualTo(contentCid);
 //        执行list
         List<TbContent> list = tbContentMapper.selectByExample(example);
-
+//        从缓存中添加内容
+        try{
+//            把list转换成字符串
+            String cacheStirng = JsonUtils.objectToJson(list);
+//            使用哈希set
+            jedisClient.hset(INDEX_CONTENT_REDIS_KEY,contentCid+"",cacheStirng);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
         return list;
 
 
