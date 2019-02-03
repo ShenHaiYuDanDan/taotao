033af4480c1b9c1c9164693fe501bc7234668417
1119778139@qq.com <qp981003>
2019-02-03 13:24:18 +0800

    删除方法

--- taotao-rests/src/main/java/com/taotao/rests/service/RedisServiceImpl.java
+++ taotao-rests/src/main/java/com/taotao/rests/service/RedisServiceImpl.java
@@ -24,8 +24,10 @@ public class RedisServiceImpl implements RedisService {
     @Override
     public TaotaoResult syncContent(long contentCid) {
         try {
+//            使用jedisClient调用删除方法hdel，命名就是删除的指令。只需要执行即可
             jedisClient.hdel(INDEX_CONTENT_REDIS_KEY, contentCid + "");
         }catch (Exception e){
+//            删除错误，返回500错误。打印异常信息。
             return TaotaoResult.build(500, ExceptionUtil.getStackTrace(e));
         }
         return TaotaoResult.ok();
