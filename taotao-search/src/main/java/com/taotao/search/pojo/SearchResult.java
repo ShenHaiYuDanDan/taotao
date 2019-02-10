699f0f6e02b81546c6d81f3a4e456d7a6d74e356
1119778139@qq.com <qp981003>
2019-02-10 14:07:10 +0800

    pojo接受实体类

---
+++ taotao-search/src/main/java/com/taotao/search/pojo/SearchResult.java
@@ -0,0 +1,50 @@
+package com.taotao.search.pojo;
+
+import java.util.List;
+
+public class SearchResult {
+//    商品列表
+    private List<Item> itemList;
+//    总记录数
+    private long   recordCount;
+//    总页数
+    private long   pageCount;
+//    当前页
+    private long  curPage;
+
+    public List<Item> getItemList() {
+        return itemList;
+    }
+
+    public SearchResult setItemList(List<Item> itemList) {
+        this.itemList = itemList;
+        return this;
+    }
+
+    public long getRecordCount() {
+        return recordCount;
+    }
+
+    public SearchResult setRecordCount(long recordCount) {
+        this.recordCount = recordCount;
+        return this;
+    }
+
+    public long getPageCount() {
+        return pageCount;
+    }
+
+    public SearchResult setPageCount(long pageCount) {
+        this.pageCount = pageCount;
+        return this;
+    }
+
+    public long getCurPage() {
+        return curPage;
+    }
+
+    public SearchResult setCurPage(long curPage) {
+        this.curPage = curPage;
+        return this;
+    }
+}
